3a7a04f0db9a076f836e2c4d4a4ab24484e0835b
lalitpandey065 <lalitpandey065@gmail.com>
2019-12-31 13:42:35 +0530

    First Commit 31-12-1992

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+
+const routes: Routes = [
+  
+  {
+    path:'',
+    pathMatch:'full',
+    redirectTo:'home'
+  },
+  {
+    path:'home',
+    pathMatch:'full',
+    component:HomeComponent
+  }
+  ,{
+  path:'products',
+  loadChildren:'./products/products.module#ProductsModule'
+}];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PmMaterialModule } from './material-module';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    PmMaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
